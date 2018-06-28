5d551c554a63855e701f3222bc74596c593e1c2d
bharadwaj <bharadwaj.j@gmail.com>
2018-06-29 00:15:20 +0530

    Get ready for deployment in prod.mydukan.org

--- src/main/java/com/my/network/socialnetwork/SwaggerConfig.java
+++ src/main/java/com/my/network/socialnetwork/SwaggerConfig.java
@@ -2,12 +2,21 @@ package com.my.network.socialnetwork;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
@@ -19,4 +28,39 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.my.network.socialnetwork.controller"))
                 .build();
     }
+
+    /*private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("MyDukan Network API")
+                .description("**Comment**")
+                .termsOfServiceUrl("**Comment**")
+                .contact("**Comment**")
+                .license("Apache License Version 2.0")
+                .licenseUrl("**Comment**").version("0.0.1")
+                .build();
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder().scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false).build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("apiKey",
+                authorizationScopes));
+    }*/
 }
